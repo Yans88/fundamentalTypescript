a51cb184dc138432e67d54da3be612729f85721a
Yansen <hanssn88@gmail.com>
2022-09-24 00:31:26 +0700

    add jawaban

--- js/app.js
+++ js/app.js
@@ -1,10 +1,5 @@
-# fundamentalTypescript
-
-Kamis, 22 Sept 2022
-
-<pre>
+"use strict";
 const nama = 'Yansen';
 const asal = 'Padang - Sumatera Barat';
 console.log('Nama Saya : ' + nama);
 console.log('Asal : ' + asal);
-</pre>
